c67da236781985b7834e18e45091255a10bcf660
jonas <skjo@itu.dk>
2023-04-22 15:09:15 +0200

    header and footer commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import Customers from './components/customers';
+import HeaderNav from './components/headerNav';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header flex-center flex-spacebetween">
-        <div className="logo flex-center">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Universitets Bøger</h1>
-        </div>
-        <div className="header-menu">
-          <ul className="flex-center">
-          <li>Link</li>
-          <li>Link</li>
-          <li>Link</li>
-          </ul>
-        </div>
-      </header>
 
+    <div className="App">
+      <HeaderNav />
+  
       <div className="container">
       <Customers />
       </div>
+
+      <Footer/>
     </div>
     
   );
---
+++ client/src/components/footer.js
@@ -0,0 +1,11 @@
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>this is the footer</div>
+        </footer>
+    );
+  }
+
+export default Footer;
