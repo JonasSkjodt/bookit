abe31f6cdb10a85f6f1d4898ce75aa26fe097736
jonas <skjo@itu.dk>
2023-05-05 13:01:34 +0200

    frontpage editing

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,11 +1,105 @@
 import React from 'react';
+import './index.css';
 
 const Home = () => {
-return (
-	<div>
-	<h1>home</h1>
-	</div>
-);
+	return (
+		<div>
+			<div className='container'>
+				<div className='section'>
+				<div className="row">
+					<div className="col s12">
+						<div className='placeholderFeatured bRad'>
+							<span>featured</span>
+						</div>
+					</div>
+				</div>
+				</div>
+
+				<div className='center'>
+					<a className="waves-effect waves-light btn-small">
+						<i className="material-icons left">cloud</i>
+						button
+					</a>
+					<a className="waves-effect waves-light btn-small">
+						<i className="material-icons left">cloud</i>
+						button
+					</a>
+					<a className="waves-effect waves-light btn-small">
+						<i className="material-icons left">cloud</i>
+						button
+					</a>
+					<a className="waves-effect waves-light btn-small">
+						<i className="material-icons left">cloud</i>
+						button
+					</a>
+				</div>
+
+				<div className='section'>
+					<div className="row">
+						<div class="col s12 m3">
+							<div class="card">
+								<div class="card-image">
+								<img src="https://unsplash.it/200/200?random"/>
+								<span class="card-title">Card Title</span>
+								</div>
+								<div class="card-content">
+								<p>book info</p>
+								</div>
+								<div class="card-action">
+								<a href="#">Book link</a>
+								</div>
+							</div>
+						</div>
+						<div class="col s12 m3">
+							<div class="card">
+								<div class="card-image">
+								<img src="https://unsplash.it/200/200?random"/>
+								<span class="card-title">Card Title</span>
+								</div>
+								<div class="card-content">
+								<p>book info</p>
+								</div>
+								<div class="card-action">
+								<a href="#">Book link</a>
+								</div>
+							</div>
+						</div>
+						<div class="col s12 m3">
+							<div class="card">
+								<div class="card-image">
+								<img src="https://unsplash.it/200/200?random"/>
+								<span class="card-title">Card Title</span>
+								</div>
+								<div class="card-content">
+								<p>book info</p>
+								</div>
+								<div class="card-action">
+								<a href="#">Book link</a>
+								</div>
+							</div>
+						</div>
+						<div class="col s12 m3">
+							<div class="card">
+								<div class="card-image">
+								<img src="https://unsplash.it/200/200?random"/>
+								<span class="card-title">Card Title</span>
+								</div>
+								<div class="card-content">
+								<p>book info</p>
+								</div>
+								<div class="card-action">
+								<a href="#">Book link</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			
+			
+			</div>
+
+		</div>
+	);
 };
 
 export default Home;
