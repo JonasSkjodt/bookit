0fe83226b209634134e6c4d9507bb612a810b9de
jonas <skjo@itu.dk>
2023-05-05 13:27:08 +0200

    frontpage editing

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -17,81 +17,141 @@ const Home = () => {
 
 				<div className='center'>
 					<a className="waves-effect waves-light btn-small">
-						<i className="material-icons left">cloud</i>
-						button
+						<i className="material-icons left">feedback</i>
+						Category
 					</a>
 					<a className="waves-effect waves-light btn-small">
-						<i className="material-icons left">cloud</i>
-						button
+						<i className="material-icons left">android</i>
+						Genre
 					</a>
 					<a className="waves-effect waves-light btn-small">
-						<i className="material-icons left">cloud</i>
-						button
-					</a>
-					<a className="waves-effect waves-light btn-small">
-						<i className="material-icons left">cloud</i>
-						button
+						<i className="material-icons left">build</i>
+						All Books
 					</a>
 				</div>
 
 				<div className='section'>
 					<div className="row">
 						<div class="col s12 m3">
-							<div class="card">
-								<div class="card-image">
-								<img src="https://unsplash.it/200/200?random"/>
-								<span class="card-title">Card Title</span>
-								</div>
-								<div class="card-content">
-								<p>book info</p>
-								</div>
-								<div class="card-action">
-								<a href="#">Book link</a>
-								</div>
-							</div>
+						<div class="card-panel teal">
+							<span class="white-text">Helper card with info.
+							</span>
 						</div>
+						</div>
+
 						<div class="col s12 m3">
-							<div class="card">
-								<div class="card-image">
-								<img src="https://unsplash.it/200/200?random"/>
-								<span class="card-title">Card Title</span>
-								</div>
-								<div class="card-content">
-								<p>book info</p>
-								</div>
-								<div class="card-action">
-								<a href="#">Book link</a>
-								</div>
+						<div class="card  z-depth-3 bRad">
+							<div class="card-image waves-effect waves-block waves-light bRadT">
+							<img class="activator" src="https://unsplash.it/250/220?random" />
+							</div>
+							<div class="card-content">
+							<span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+							<p><a href="#">This is a link</a></p>
+							</div>
+							<div class="card-reveal">
+							<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+							<p>Here is some more information about this product that is only revealed once clicked on.</p>
 							</div>
 						</div>
+						</div>
+
 						<div class="col s12 m3">
-							<div class="card">
-								<div class="card-image">
-								<img src="https://unsplash.it/200/200?random"/>
-								<span class="card-title">Card Title</span>
-								</div>
-								<div class="card-content">
-								<p>book info</p>
-								</div>
-								<div class="card-action">
-								<a href="#">Book link</a>
-								</div>
+						<div class="card  z-depth-3 bRad">
+							<div class="card-image waves-effect waves-block waves-light bRadT">
+							<img class="activator" src="https://unsplash.it/250/220?random" />
+							</div>
+							<div class="card-content">
+							<span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+							<p><a href="#">This is a link</a></p>
+							</div>
+							<div class="card-reveal">
+							<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+							<p>Here is some more information about this product that is only revealed once clicked on.</p>
 							</div>
 						</div>
+						</div>
+
 						<div class="col s12 m3">
-							<div class="card">
-								<div class="card-image">
-								<img src="https://unsplash.it/200/200?random"/>
-								<span class="card-title">Card Title</span>
-								</div>
-								<div class="card-content">
-								<p>book info</p>
-								</div>
-								<div class="card-action">
-								<a href="#">Book link</a>
-								</div>
+						<div class="card  z-depth-3 bRad">
+							<div class="card-image waves-effect waves-block waves-light bRadT">
+							<img class="activator" src="https://unsplash.it/250/220?random" />
+							</div>
+							<div class="card-content">
+							<span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+							<p><a href="#">This is a link</a></p>
+							</div>
+							<div class="card-reveal">
+							<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+							<p>Here is some more information about this product that is only revealed once clicked on.</p>
 							</div>
 						</div>
+						</div>
+
+						<div class="col s12 m3">
+						<div class="card  z-depth-3 bRad">
+							<div class="card-image waves-effect waves-block waves-light bRadT">
+							<img class="activator" src="https://unsplash.it/250/220?random" />
+							</div>
+							<div class="card-content">
+							<span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+							<p><a href="#">This is a link</a></p>
+							</div>
+							<div class="card-reveal">
+							<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+							<p>Here is some more information about this product that is only revealed once clicked on.</p>
+							</div>
+						</div>
+						</div>
+
+						<div class="col s12 m3">
+						<div class="card  z-depth-3 bRad">
+							<div class="card-image waves-effect waves-block waves-light bRadT">
+							<img class="activator" src="https://unsplash.it/250/220?random" />
+							</div>
+							<div class="card-content">
+							<span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+							<p><a href="#">This is a link</a></p>
+							</div>
+							<div class="card-reveal">
+							<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+							<p>Here is some more information about this product that is only revealed once clicked on.</p>
+							</div>
+						</div>
+						</div>
+
+						<div class="col s12 m3">
+						<div class="card  z-depth-3 bRad">
+							<div class="card-image waves-effect waves-block waves-light bRadT">
+							<img class="activator" src="https://unsplash.it/250/220?random" />
+							</div>
+							<div class="card-content">
+							<span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+							<p><a href="#">This is a link</a></p>
+							</div>
+							<div class="card-reveal">
+							<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+							<p>Here is some more information about this product that is only revealed once clicked on.</p>
+							</div>
+						</div>
+						</div>
+
+						<div class="col s12 m3">
+						<div class="card  z-depth-3 bRad">
+							<div class="card-image waves-effect waves-block waves-light bRadT">
+							<img class="activator" src="https://unsplash.it/250/220?random" />
+							</div>
+							<div class="card-content">
+							<span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+							<p><a href="#">This is a link</a></p>
+							</div>
+							<div class="card-reveal">
+							<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+							<p>Here is some more information about this product that is only revealed once clicked on.</p>
+							</div>
+						</div>
+						</div>
+
+						
 					</div>
 				</div>
 			
