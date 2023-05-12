e1ff7ba2e16e21c75e2ed5e1ea865cfb6e74fe74
jonas <skjo@itu.dk>
2023-05-12 12:10:57 +0200

    footer image and links skeleton, app.post for post

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,9 +1,25 @@
 import './footer.css';
-
+//<!-- Change the social media icons with images from /public -->
 const Footer = () => {
     return (
-        <footer>
-            <div className='container'>this is the footer</div>
+        <footer className="flex-it flex-align-center">
+            
+            <div className='container'>
+                <div className="flex-it flex-align-center flex-justify">
+                    <div className="">
+                        <img src="/logo192.png" alt=""/>
+                    </div>
+                    <div className="">
+                        <ul>
+                            <li>
+                            <a href="#" rel="" className="waves-effect waves-light">
+								<i className="material-icons">Mail</i>
+                            </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
   }
