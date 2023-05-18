c5c429a7aa830c8830297ec531e78a6b2bc21255
jonas <skjo@itu.dk>
2023-05-18 12:45:26 +0200

    contact + sign up

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 const Contact = () => {
 return (
-	<div>
+	<div className="container">
 	<h1>contact</h1>
+	<p>Contact us if you have any questions or need further help</p>
+	<div className="row">
+    <form className="col s12">
+      <div className="row">
+        <div className="input-field col s6">
+          <i className="material-icons prefix">mode_edit</i>
+          <textarea id="icon_prefix2" className="materialize-textarea white-text"></textarea>
+          <label for="icon_prefix2">Subject</label>
+        </div>
+      </div>
+	  <div className="row">
+        <div className="input-field col s12">
+          <textarea id="textarea1" className="materialize-textarea white-text"></textarea>
+          <label for="textarea1">Your message</label>
+        </div>
+      </div>
+    </form>
+  </div>
 	</div>
 );
 };
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -2,32 +2,32 @@ import React from 'react';
 
 const SignUp = () => {
 return (
-	<div class="container">
+	<div className="container">
         <h1>Sign up</h1>
-		<p>sign up to enjoy the benefits of BookIT</p>
-	<div class="row">
-    <form class="col s12">
-      <div class="row">
-        <div class="input-field col s6">
-          <input id="first_name" type="text" class="validate" />
+		<p>Sign up to enjoy the benefits of BookIT</p>
+	<div className="row">
+    <form className="col s12">
+      <div className="row">
+        <div className="input-field col s6">
+          <input id="first_name" type="text" className="validate white-text" />
           <label for="first_name">First Name</label>
         </div>
-        <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate" />
+        <div className="input-field col s6">
+          <input id="last_name" type="text" className="validate white-text" />
           <label for="last_name">Last Name</label>
         </div>
       </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="password" type="password" class="validate" />
+      <div className="row">
+        <div className="input-field col s12">
+          <input id="password" type="password" className="validate white-text" />
           <label for="password">Password</label>
         </div>
       </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="email" type="email" class="validate" />
+      <div className="row">
+        <div className="input-field col s12">
+          <input id="email" type="email" className="validate white-text" />
           <label for="email">Email</label>
-          <span class="helper-text" data-error="wrong" data-success="right">Please insert a correct email</span>
+          <span className="helper-text" data-error="Please insert a correct email to sign up" data-success="right">Please insert a correct email</span>
         </div>
       </div>
     </form>
