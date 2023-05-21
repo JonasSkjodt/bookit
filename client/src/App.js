c23811625549f81428371d13a14d9cbc64b30321
jonas <skjo@itu.dk>
2023-05-21 10:46:01 +0200

    continued coding

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 import { BrowserRouter as Router, Routes, Route}
 	from 'react-router-dom';
 import Home from './pages';
