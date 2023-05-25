738a29e7ac3c4924c5ff4a8d48bc120f31b373c6
Theis <thph@itu.dk>
2023-05-25 13:52:06 +0200

    Added some more things to contact us page

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -14,7 +14,7 @@ return (
     <div className="section">
       <div className="row container">
         <h2 className="header our_goal">The Goal</h2>
-        <p className="grey-text text-darken-3 lighten-3 paragraphText">Our goal with BookIT has been and always will be to allow students to buy and sell academic literature fast and easily. We believe it's everybody should have the
+        <p className="paragraphText">Our goal with BookIT has been and always will be to allow students to buy and sell academic literature fast and easily. We believe it's everybody should have the
         right to study only based on their knowledge and hard work. Sadly for many a roadblock will be the price of academic books and literature. We here at BookIT do not believe that the defining factor for wether a student gets to follow their dreams should be
         their wealth. We want to ensure that all students can get required reading material for courses as cheap as possible whilst also allowing students to recoup cost from already owned books. This helps create a 
         circular economy where students help and thrive off each other creating more opportunities for everyone </p>
@@ -24,7 +24,7 @@ return (
 	<div className="section">
       <div className="row container">
         <h2 className="header our_goal">The Story</h2>
-        <p className="grey-text text-darken-3 lighten-3 paragraphText">BookIT started as a small business catered towards students who didn't want to spend too much on academic books. The founder would
+        <p className="paragraphText">BookIT started as a small business catered towards students who didn't want to spend too much on academic books. The founder would
         go around on campus and post online trying to coordinate book trades between students. It eventually got so popular that he decided to launch his own website and BookIT is now one of the 
         most widely used book trading and selling sites for students across the country.</p>
       </div>
