5dc996c756517414889c97fefeed0e8773aa1a5f
Theis <thph@itu.dk>
2023-05-25 12:31:20 +0200

    First version of about page and started work on contact us

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import './contact.css';
+
 
 const Contact = () => {
 return (
 	<div className="container">
-	<h1>contact</h1>
+  <div className="section FAQ white">
+    <div className="row container"> 
+      <div className="col s12 m6">
+        <h3 className="header">FAQ</h3>
+        <ul class="collapsible">
+    <li>
+      <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+  </ul>
+      </div>
+    </div>
+  </div>
+
+	<h1>Contact</h1>
 	<p>Contact us if you have any questions or need further help</p>
 	<div className="row">
     <form className="col s12">
