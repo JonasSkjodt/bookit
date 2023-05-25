9d2b9abce6e68260a2ba819e8249a2096ff9507a
Theis <thph@itu.dk>
2023-05-25 14:41:45 +0200

    Added the logo

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -5,6 +5,14 @@ import './contact.css';
 const Contact = () => {
 return (
 	<div className="container">
+  <div className="section">
+    <h3 className="header center">Contact us</h3>
+    <p>Below you will find a small FAQ that may answer some of your questions. If you still need help after checking this out you can either contact one of our support staff directly
+      or you can fill out the form below and we will get back to you as soon as possible.
+    </p>
+  </div>
+  <div className="section spacer">
+  </div>
   <div className="section FAQ white">
         <h3 className="header center">FAQ</h3>
     <div className="row container"> 
@@ -21,7 +29,7 @@ return (
     </li>
     </ul>
     <ul className="collapsible expandable">
-    <li>
+      <li>
       <div className="collapsible-header"><i className="material-icons">attach_money</i>Payment options & pricing?</div>
       <div className="collapsible-body"><span>
         <h5>Payment options</h5>
@@ -32,13 +40,13 @@ return (
         the seller will be notified that they should lower the price. If the book is not sold within 60 days the seller will be notified again and the book will be removed from the site. If you have any questions
         regarding the pricing of a product please contact the seller as we do not have any direct control over this.
          </span></div>
-    </li>
+      </li>
     </ul>
     <ul className="collapsible expandable">
-    <li>
+      <li>
       <div className="collapsible-header"><i className="material-icons">whatshot</i>Third</div>
       <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-    </li>
+      </li>
     </ul>
       </div>
       <div className="col s12 m6">
@@ -68,31 +76,31 @@ return (
 
   <div className="row container">
     <div className="col s12 m6">
-      <div className="section white">
+      <div className="section white employees_section">
         <div className="row">
           <div className="col s12 m6">
-            <img className="employees" src="https://images.unsplash.com/photo-1618568949779-895d81686151?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1742&q=80"></img>
+            <img className="employees employees_John" src="https://images.unsplash.com/photo-1618568949779-895d81686151?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1742&q=80"></img>
           </div>
           <div className="col s12 m6 employees_text">
             <h4>John Doe</h4>
             <p>IT Expert</p>
-            <p>Tlf: 12 34 56 78</p>
-            <p>Email: JohnDoe@BookIT.com</p>
+            <p>Tel: <a href="tel: +45 12 34 56 78">12 34 56 78</a> </p>
+            <p>Email: <a href="mailto: JohnDoe@BookIT.com">JohnDoe@BookIT.com</a> </p>
           </div>
         </div>
       </div>
     </div>
     <div className="col s12 m6">
-      <div className="section white">
+      <div className="section white employees_section">
       <div className="row">
           <div className="col s12 m6">
-            <img className="employees" src="https://images.unsplash.com/photo-1618568949779-895d81686151?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1742&q=80"></img>
+            <img className="employees employees_Seb" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"></img>
           </div>
           <div className="col s12 m6 employees_text">
             <h4>Sebastian Holst</h4>
             <p>Warehouse</p>
-            <p>Tlf: 87 65 43 21</p>
-            <p>Email: SebHol@BookIT.com</p>
+            <p>Tel: <a href="tel: +45 87 65 43 21">87 65 43 21</a></p>
+            <p>Email: <a href="mailto: SebHol@BookIT.com">SebHol@BookIT.com</a> </p>
           </div>
         </div>
       </div>    
@@ -102,6 +110,13 @@ return (
 	<p>Contact us if you have any questions or need further help</p>
 	<div className="row">
     <form className="col s12">
+    <div className="row">
+        <div className="input-field col s6">
+          <i className="material-icons prefix">mail</i>
+          <textarea id="icon_prefix2" className="materialize-textarea white-text"></textarea>
+          <label htmlFor="icon_prefix2">Email</label>
+        </div>
+      </div>
       <div className="row">
         <div className="input-field col s6">
           <i className="material-icons prefix">mode_edit</i>
@@ -116,6 +131,9 @@ return (
         </div>
       </div>
     </form>
+    <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+    <i class="material-icons right">send</i>
+  </button>
   </div>
 	</div>
 );
