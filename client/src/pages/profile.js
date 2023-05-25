92149d259c627149c1b3b15705a5889a1d502a72
jonas <skjo@itu.dk>
2023-05-25 13:06:28 +0200

    primitive search function

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Profile = () => {
 return (
-	<div>
+	<div className="container">
 	<h1>Profile page</h1>
 	</div>
 );
--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import { React, useState } from "react";
+import List from "./list";
 
 const Search = () => {
+	const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
 return (
 	<div className="container">
 	<h1>search page</h1>
-	</div>
+	<div className="search">
+        <input id="outlined-basic" className="white-text" onChange={inputHandler} variant="outlined" fullWidth label="Search"/>
+    </div>
+    <List input={inputText} />
+    </div>
 );
 };
 
