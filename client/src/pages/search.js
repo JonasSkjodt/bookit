a3f1771e5925eef9dfd67c5d1011f3529b6ce575
jonas <skjo@itu.dk>
2023-05-25 13:32:04 +0200

    search continued

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -2,21 +2,23 @@ import { React, useState } from "react";
 import List from "./list";
 
 const Search = () => {
-	const [inputText, setInputText] = useState("");
-  let inputHandler = (e) => {
+
+  const [inputText, setInputText] = useState("");
+  	let inputHandler = (e) => {
     //convert input text to lower case
     var lowerCase = e.target.value.toLowerCase();
     setInputText(lowerCase);
   };
-return (
-	<div className="container">
-	<h1>search page</h1>
-	<div className="search">
-        <input id="outlined-basic" className="white-text" onChange={inputHandler} variant="outlined" fullWidth label="Search"/>
-    </div>
-    <List input={inputText} />
-    </div>
-);
+
+	return (
+		<div className="container">
+			<h1>search page</h1>
+			<div className="search">
+				<input id="outlined-basic" className="white-text" onChange={inputHandler} variant="outlined" fullWidth label="Search"/>
+			</div>
+			<List input={inputText} />
+		</div>
+	);
 };
 
 export default Search;
