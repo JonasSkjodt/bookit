a5538de51a8cbe16514fd28f44cc57df2fadd8d1
jonas <skjo@itu.dk>
2023-05-28 11:32:11 +0200

    profile continued

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -120,33 +120,42 @@ const Profile = () => {
 								for(const element of itemIDs) {
 									const customer = customers.find(customer => customer.id === element);
 									books.push(
-									<div key={customer.id} className="col s6 m3">
-								<div className="card bRad">
-									<div className="card-image waves-effect waves-block waves-light bRadT">
-									<img className="activator" src={customer.image} />
-									</div>
-									<div className="card-content">
-									<span className="card-title activator white-text text-darken-4">
-									{customer.bookName}<i className="material-icons right">more_vert</i>
-										</span>
-										<p><a className="book-tag" href="#">{customer.tags}</a></p>
-										<a className="btn-floating halfway-fab waves-effect waves-light red">
-										<i className="material-icons">favorite</i></a>
-									</div>
-									<div className="card-reveal">
-									<span className="card-title grey-text text-darken-4">{customer.bookName}<i className="material-icons right">close</i></span>
-									<p className="black-text">{customer.bookName} <span>{customer.condition}</span></p>
-									<Link
-										to = "/product"
-										state={{
-											book: customer
-										}}
-										>
-										Product page
-									</Link>
-									</div>
-								</div>
-							</div>
+										<div key={customer.id} className="col s6 m4 l3">
+										<div className="card bRad book-shadow">
+												<div className="card-image waves-effect waves-block waves-light bRadT">
+												<img className="activator" src="https://source.unsplash.com/random/300×330/?bird" alt="book" />
+												</div>
+												<div className="card-content">
+												<span className="card-title activator white-text text-darken-4 text-overflow">
+												{customer.bookName}<i className="material-icons right">more_vert</i>
+												  </span>
+												  <p>
+												  <Link
+												  className="book-tag"
+												  to = "/product"
+												  state={{
+													book: customer
+												  }}
+												  ><i className="material-icons">shopping_cart</i> Buy Now</Link>
+												  </p>
+												  <a className="btn-floating halfway-fab waves-effect waves-light">
+												  {customer.price} Kr.</a>
+												</div>
+												<div className="card-reveal">
+												<span className="card-title grey-text text-darken-4">{customer.bookName}<i className="material-icons right">close</i></span>
+												<p className="black-text">ISBN: {customer.isbn}</p>
+												<p className="black-text">Condition: {customer.condition}</p>
+												<Link
+												  to = "/product"
+												  state={{
+													book: customer
+												  }}
+												  >
+												  See product page
+												</Link>
+												</div>
+											  </div>
+											  </div>
 						);
 							}
 							return books;
